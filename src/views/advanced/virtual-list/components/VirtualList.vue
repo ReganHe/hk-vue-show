2dd42450133ac6e6ece5a7dccac50820a01522c4
ReganHe <heoo547627@126.com>
2024-01-26 12:56:04 +0800

    集成虚拟列表功能-功能开发

---
+++ src/views/advanced/virtual-list/components/VirtualList.vue
@@ -0,0 +1,126 @@
+<template>
+  <div
+    class="scroll-box"
+    ref="scrollBox"
+    @scroll="handleScroll"
+    :style="{ height: scrollHeight + 'px' }"
+  >
+    <div
+      class="virtual-list"
+      :style="{ paddingTop: paddingTop + 'px', paddingBottom: paddingBottom + 'px' }"
+    >
+      <div
+        v-for="(item, index) in visibleItems"
+        :key="index"
+        :style="{ height: itemHeight + 'px' }"
+      >
+        <slot name="item" :item="item" :index="index"></slot>
+      </div>
+    </div>
+    <Loading :is-show="isShowLoad" />
+  </div>
+</template>
+
+<script setup>
+  import { ref, computed, onMounted, onUnmounted } from 'vue';
+  import { message } from 'ant-design-vue';
+  import Loading from './Loading.vue';
+
+  const props = defineProps({
+    listData: { type: Array, default: () => [] },
+    itemHeight: { type: Number, default: 50 },
+    scrollHeight: { type: Number, default: 300 },
+    loadMore: { type: Function, required: true },
+  });
+
+  const isShowLoad = ref(false);
+
+  const visibleCount = Math.ceil(props.scrollHeight / props.itemHeight) + 1;
+  const start = ref(0);
+  const end = computed(() => Math.min(start.value + 2 * visibleCount - 1, renderData.value.length));
+  const paddingTop = computed(() => start.value * props.itemHeight);
+  const renderData = ref([...props.listData]);
+  const paddingBottom = computed(() => (renderData.value.length - end.value) * props.itemHeight);
+  const visibleItems = computed(() => renderData.value.slice(start.value, end.value));
+  const scrollBox = ref(null);
+  let lastIndex = start.value;
+
+  const handleScroll = rafThrottle(() => {
+    onScrollToBottom();
+    onScrolling();
+  });
+
+  const onScrolling = () => {
+    const scrollTop = scrollBox.value.scrollTop;
+    let thisStartIndex = Math.floor(scrollTop / props.itemHeight);
+    const isSomeStart = thisStartIndex == lastIndex;
+    if (isSomeStart) return;
+    const isEndIndexOverListLen = thisStartIndex + 2 * visibleCount - 1 >= renderData.value.length;
+    if (isEndIndexOverListLen) {
+      thisStartIndex = renderData.value.length - (2 * visibleCount - 1);
+    }
+    lastIndex = thisStartIndex;
+    start.value = thisStartIndex;
+  };
+
+  const onScrollToBottom = () => {
+    const scrollTop = scrollBox.value.scrollTop;
+    const clientHeight = scrollBox.value.clientHeight;
+    const scrollHeight = scrollBox.value.scrollHeight;
+    if (scrollTop + clientHeight >= scrollHeight) {
+      loadMore();
+    }
+  };
+  let loadingLock = false;
+  let lockLoadMoreByHideLoading_once = false;
+  const loadMore = async () => {
+    if (loadingLock) return;
+    if (lockLoadMoreByHideLoading_once) {
+      lockLoadMoreByHideLoading_once = false;
+      return;
+    }
+    loadingLock = true;
+    isShowLoad.value = true;
+    const moreData = await props.loadMore().catch((err) => {
+      console.error(err);
+      message.success('获取数据失败，请检查网络后重试');
+      return [];
+    });
+    if (moreData.length != 0) {
+      renderData.value = [...renderData.value, ...moreData];
+      handleScroll();
+    }
+    isShowLoad.value = false;
+    lockLoadMoreByHideLoading_once = true;
+    loadingLock = false;
+  };
+
+  function rafThrottle(fn) {
+    let lock = false;
+    return function (...args) {
+      if (lock) return;
+      lock = true;
+      window.requestAnimationFrame(() => {
+        fn.apply(args);
+        lock = false;
+      });
+    };
+  }
+
+  onMounted(() => {
+    scrollBox.value.addEventListener('scroll', handleScroll);
+  });
+
+  onUnmounted(() => {
+    scrollBox.value.removeEventListener('scroll', handleScroll);
+  });
+</script>
+
+<style lang="scss" scoped>
+  .virtual-list {
+    position: relative;
+  }
+  .scroll-box {
+    overflow-y: auto;
+  }
+</style>
--- src/views/advanced/virtual-list/index.vue
+++ src/views/advanced/virtual-list/index.vue
@@ -1,7 +1,65 @@
 <template>
-  <div class="virtual-list">virtual-list</div>
+  <!-- class="virtualContainer" -->
+  <VirtualList
+    v-if="listData.length > 0"
+    :listData="listData"
+    :itemHeight="50"
+    :scrollHeight="600"
+    :loadMore="loadMore"
+  >
+    <template #item="{ item }">
+      <div class="list-item">
+        {{ item }}
+      </div>
+    </template>
+  </VirtualList>
 </template>
 
-<script setup></script>
+<script setup>
+  import { onMounted, ref } from 'vue';
+  import VirtualList from './components/VirtualList.vue';
 
-<style lang="scss" scoped></style>
+  let listData = ref([]);
+  let num = 200;
+  // 模拟从 API 获取数据
+  const fetchData = async () => {
+    // 这里可以替换成您实际的 API 请求
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        const newData = Array.from({ length: 200 }, (_, index) => `Item ${index}`);
+        listData.value = newData;
+        resolve();
+      }, 500);
+    });
+  };
+  // 加载更多数据
+  const loadMore = () => {
+    // 这里可以替换成您实际的 API 请求
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const moreData = Array.from({ length: 30 }, (_, index) => `Item ${num + index}`);
+        num += 30;
+        resolve(moreData);
+      }, 500);
+    });
+    //模拟请求错误
+    // return new Promise((_, reject) => {
+    //   setTimeout(() => {
+    //     reject('错误模拟');
+    //   }, 1000);
+    // })
+  };
+
+  onMounted(() => {
+    fetchData();
+  });
+</script>
+
+<style scoped>
+  .list-item {
+    height: 50px;
+    line-height: 50px;
+    border-bottom: 1px solid #ccc;
+    text-align: center;
+  }
+</style>
